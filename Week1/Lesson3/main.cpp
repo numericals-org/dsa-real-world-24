c42121a79c55adef387bf66b6118595c8ccfcafd
Shivam Pandey <shivamdreams123@gmail.com>
2026-08-31 12:25:47 +0530

    Built a complete dynamic array from scratch

--- Week1/Lesson3/main.cpp
+++ Week1/Lesson3/main.cpp
@@ -12,12 +12,23 @@ private:
 public:
     Array(int cap)
     {
-        if(cap < 0)
+        if (cap < 0)
             throw std::invalid_argument("Capacity must be positive!");
 
         data = new int[cap];
         capacity = cap;
         length = 0;
+    }
+    Array(const Array &other)
+    {
+        capacity = other.capacity;
+        length = other.length;
+        data = new int[capacity];
+
+        for (int i = 0; i < length; i++)
+        {
+            data[i] = other.data[i];
+        }
     }
     ~Array()
     {
@@ -41,50 +52,84 @@ public:
     }
     void insert(int index, int value)
     {
-        if (index < 0|| index > length)
+        if (index < 0 || index > length)
             throw std::out_of_range("Index is out of bounds!");
-        if (length == capacity){
+        if (length == capacity)
+        {
             resize();
         }
-        for(int i = length - 1; i >= index; i--){
-            data[i+1] = data[i];
+        for (int i = length - 1; i >= index; i--)
+        {
+            data[i + 1] = data[i];
         }
         data[index] = value;
         length++;
     }
-    void resize(){
-        if (capacity == 0){
+    void resize()
+    {
+        if (capacity == 0)
+        {
             capacity = 1;
-        } 
-       auto copyData  =  new int[capacity*2];
-       for(int i=0;i<length;i++){
-           copyData[i] = data[i];
-       }
+        }
+        auto copyData = new int[capacity * 2];
+        for (int i = 0; i < length; i++)
+        {
+            copyData[i] = data[i];
+        }
 
-       delete[] data;
-       data = copyData;
-       capacity *= 2;
+        delete[] data;
+        data = copyData;
+        capacity *= 2;
     }
-    void remove(int index){
-        if (index < 0|| index >= length)
+    void remove(int index)
+    {
+        if (index < 0 || index >= length)
             throw std::out_of_range("Index is out of bounds!");
-        for(int i = index; i < length-1; i++){
-            data[i] = data[i+1];
+        for (int i = index; i < length - 1; i++)
+        {
+            data[i] = data[i + 1];
         }
         length--;
     }
-    void shrink_to_fit(){
-        if(length == 0){
-             throw std::logic_error("This Operation is not possible");
+    void shrink_to_fit()
+    {
+        if (length == 0)
+        {
+            throw std::logic_error("This Operation is not possible");
         }
-        auto copyData  =  new int[length];
-        for(int i=0;i<length;i++){
+        auto copyData = new int[length];
+        for (int i = 0; i < length; i++)
+        {
             copyData[i] = data[i];
         }
 
         delete[] data;
         data = copyData;
         capacity = length;
+    }
+    int search(int value)
+    {
+        for (int i = 0; i < length; i++)
+        {
+            if (data[i] == value)
+            {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+    Array searchAll(int value)
+    {
+        Array returnValue(2);
+        for (int i = 0; i < length; i++)
+        {
+            if (data[i] == value)
+            {
+                returnValue.insert(returnValue.size(), i);
+            }
+        }
+        return returnValue;
     }
     // We will build these together step-by-step
 };
