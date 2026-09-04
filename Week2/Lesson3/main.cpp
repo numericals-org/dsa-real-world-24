a4876b2c3219024d3434a0960559f9bc9b65c8fa
Shivam Pandey <shivamdreams123@gmail.com>
2026-09-04 13:33:53 +0530

    add findFirstOccurrence, findLastOccurrence, lowerBound, upperBound

--- Week2/Lesson3/main.cpp
+++ Week2/Lesson3/main.cpp
@@ -2,19 +2,103 @@
 #include <vector>
 using namespace std;
 
-int binarySearch(const vector<int>& nums, int target){
+int binarySearch(const vector<int> &nums, int target)
+{
     int left = 0, right = static_cast<int>(nums.size() - 1);
-    
-    while(left <= right){
+
+    while (left <= right)
+    {
         int mid = left + (right - left) / 2;
-        if(nums[mid] == target) return mid;
-        if(nums[mid]<target) left = mid + 1;
-        else right = mid - 1;
+        if (nums[mid] == target)
+            return mid;
+        if (nums[mid] < target)
+            left = mid + 1;
+        else
+            right = mid - 1;
     }
 
     return -1;
 }
 
-int main() {
+int findFirstOccurrence(const std::vector<int> &nums, int target)
+{
+    int left = 0, right = static_cast<int>(nums.size() - 1), ans = -1;
+
+    while (left <= right)
+    {
+        int mid = left + (right - left) / 2;
+        if (nums[mid] == target)
+        {
+            ans = mid;
+            right = mid - 1;
+        }
+        else if (nums[mid] < target)
+            left = mid + 1;
+        else
+            right = mid - 1;
+    }
+    return ans;
+}
+
+int findLastOccurrence(const std::vector<int> &nums, int target)
+{
+    int left = 0, right = static_cast<int>(nums.size() - 1), ans = -1;
+
+    while (left <= right)
+    {
+        int mid = left + (right - left) / 2;
+        if (nums[mid] == target)
+        {
+            ans = mid;
+            left = mid + 1;
+        }
+        else if (nums[mid] < target)
+            left = mid + 1;
+        else
+            right = mid - 1;
+    }
+    return ans;
+}
+
+int lowerBound(const std::vector<int> &nums, int target)
+{
+    int left = 0, right = static_cast<int>(nums.size() - 1), ans = -1;
+
+    while (left <= right)
+    {
+        int mid = left + (right - left) / 2;
+        if (nums[mid] >= target)
+        {
+            ans = mid;
+            right = mid - 1;
+        }
+        else
+            left = mid + 1;
+    }
+
+    return ans;
+}
+
+int upperBound(const std::vector<int> &nums, int target)
+{
+    int left = 0, right = static_cast<int>(nums.size() - 1), ans = -1;
+
+    while (left <= right)
+    {
+        int mid = left + (right - left) / 2;
+        if (nums[mid] > target)
+        {
+            ans = mid;
+            right = mid - 1;
+        }
+        else
+            left = mid + 1;
+    }
+
+    return ans;
+}
+
+int main()
+{
     return 0;
 }
